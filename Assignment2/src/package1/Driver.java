c62562e2f83848f68ece65c735baa88d23f60aa9
Ferdousara Parvin <tlg.mzlleferdou_8@hotmail.com>
2018-02-21 00:13:06 -0500

    only copyFestival left to do

--- Assignment2/src/package1/Driver.java
+++ Assignment2/src/package1/Driver.java
@@ -7,15 +7,28 @@ import package4.Fair;
 public class Driver {
 
     static MusicFiesta mf1; // Created to test equality of two objects
-    
+
     //TODO: PUt constants
     static final int ZERO = 0;
 
+    public static Event[] copyFestival(Event[] eventArray) {
+        return;
+    }
+    
     public static void main(String[] args) {
 
-        //An Event List created to test the toString and equals method
+        // Print a welcome message
+        System.out.println("--------------------------------------\n"
+                + " Welcome to Viveka and Ferdousara's program\n"
+                + "--------------------------------------------");
+
+        System.out.println("\n--------------------------------PART I-------------------------------");
+
+        // ****Part 1.1
+        // Testing toString method
+        System.out.println();
         System.out.println("**************************");
-        System.out.println("Events created in list1");
+        System.out.println("Testing toString method");
         System.out.println("**************************\n");
 
         //Creation of 6 different events
@@ -28,16 +41,20 @@ public class Driver {
             new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE)
         };
 
-        //Display each event in list1
+        // Print each event in list1
         for (Event e : list1) {
             System.out.println(e);
             System.out.println("----------------------------------------------------------------------");
         }
 
         // Testing equality
+        System.out.println("\n**************************");
+        System.out.println("Testing equals method");
+        System.out.println("**************************\n");
+
         Festival fe1 = new Festival("arts", 20, 30, 2015, 9, 2);
         Fair fa1 = new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE);
-        Fair fa2 = new Fair(23, 2019, 6, 2, Fair.TypeOfFair.SCIENCE); 
+        Fair fa2 = new Fair(23, 2019, 6, 2, Fair.TypeOfFair.SCIENCE);
 
         System.out.println("Are these equal? " + list1[0].equals(fe1)); // Two different type of objects
         System.out.println("Are these equal? " + list1[3].equals(mf1)); // One object is null
@@ -45,32 +62,37 @@ public class Driver {
         System.out.println("Are these equal? " + list1[5].equals(fa2)); // Not equal objects
         System.out.println("Are these equal? " + list1[1].equals((Festival) list1[3])); // Two different type of objects even when casted
 
-        //Event List with 10 Event objects
-        System.out.println("**************************");
-        System.out.println("Events created in list2");
-        System.out.println("**************************\n");
+        System.out.println();
 
-        //Creats events and add them to list2
+        // *****Part 1.2
+        // Creats events and add them to list2
         Event[] list2 = {
             new Event(list1[0]),
             new Festival(fe1), // Use of copy cstor
             new CulturalFiesta((CulturalFiesta) list1[2]),
             new MusicFiesta((MusicFiesta) list1[3]),
             new SportCompetition((SportCompetition) list1[4]),
-            new Fair(fa1), // Use of copy constructor
+            new Fair(fa1), // Use of copy cstor
             new Event(2018, 9, 2),
             new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE),
             new Festival("science", 23, 8, 2019, 1, 2),
             new SportCompetition(2, 2018, 8, 4, SportCompetition.Season.SUMMER)
         };
 
-        //Display events in list2 and seperate them with a divider (Purpose of this for loop: for clarity only)
+        System.out.println("//Array of 10 Event objects//");
+
+        // Display events in list2 and seperate them with a divider (Purpose of this for loop: for clarity only)
         for (int i = ZERO; i < list2.length; i++) {
             System.out.println("#" + i + ". " + list2[i]);
             System.out.println("----------------------------------------------------------------------");
         }
 
-        //Determine the most "popular" event
+        //-------------Most/least amount of cities-------------------//
+        System.out.println("\n*************************************************");
+        System.out.println("Finding events with least/most amount of cities");
+        System.out.println("*************************************************\n");
+
+        // Determine the most "popular" event
         int leastPopular = ZERO;
         int mostPopular = ZERO;
 
@@ -86,8 +108,8 @@ public class Driver {
             }
         }
 
-        String output1 = "The event that will include the least amount of cities: ";
-        String output2 = "The event that will include the most amount of cities: ";
+        String output1 = "The event(s) that will include the least amount of cities: ";
+        String output2 = "The event(s) that will include the most amount of cities: ";
 
         //Iterate through the list and display all the events that are the least and most popular
         for (int i = ZERO; i < list2.length; i++) {
@@ -102,7 +124,94 @@ public class Driver {
 
         System.out.println(output1 + "\n" + output2);
 
-        String output3 = "Events #";
+        //---------------Events held the same year ---------------------------//
+        System.out.println("\n*************************************************");
+        System.out.println("Finding events held the same year");
+        System.out.println("*************************************************\n");
+
+        int[] yearWithMultipleEventsAndAlreadyPrinted = {ZERO, ZERO, ZERO, ZERO}; // Since in list2, there are four different possibility of years (2008, 2015, 2018, and 2019)
+        int index = ZERO;
+
+        // Loop through list2
+        for (int i = ZERO; i < list2.length; i++) {
+
+            String output = "The following events will be held in the same year: #" + i;
+            int numberOfMatch = ZERO; // initialize to 0 everytime we loop in the main for loop
+
+            // Compare every event's year to every other event's year except its own
+            for (int j = ZERO; j < list2.length; j++) {
+                if (list2[i].getYear() == list2[j].getYear() && i != j) { // i != j is written so that we do not ocmpare an event to itself
+                    output += " #" + j; // Add the event's index in the list2 array to the string output
+                    numberOfMatch++; // to keep track of how many matches we have for the same year
+                }
+            }
+
+            // Events occuring in the same year are printed if there is at least one match (two events in the same year) and if that year has not been printed yet
+            if (numberOfMatch > ZERO) { // Two events or more have the same year
+
+                boolean isAMatch = false; // initialize isAMath to false before going through the if statement
+
+                for (int k = ZERO; k < yearWithMultipleEventsAndAlreadyPrinted.length; k++) {
+                    if (list2[i].getYear() == yearWithMultipleEventsAndAlreadyPrinted[k]) { // that year has already been printed
+                        isAMatch = true;
+                    }
+                }
+
+                if (!isAMatch) { // Will only go through this if statement if the specific year has not been printed yet
+                    System.out.println(output + " which is in " + list2[i].getYear()); // Print output
+                    yearWithMultipleEventsAndAlreadyPrinted[index] = list2[i].getYear(); // Add the year to the array yearWithMultipleEventsAndAlreadyPrinted to remember that we already printed all the events happening that year
+                    index++; // to add the next year to the next index of array yearWithMultipleEventsAndAlreadyPrinted
+                }
+
+            }
+        }
+
+        System.out.println("\n--------------------------------PART II-------------------------------");
+
+        System.out.println("\n**************************");
+        System.out.println("Testing copyfestival");
+        System.out.println("**************************\n");
+
+        System.out.println("//Array of 12 Event objects//");
+
+        Event[] list3 = {
+            new Event(list1[0]),
+            new Festival(fe1), // Use of copy cstor
+            new CulturalFiesta((CulturalFiesta) list1[2]),
+            new MusicFiesta((MusicFiesta) list1[3]),
+            new SportCompetition((SportCompetition) list1[4]),
+            new Fair(fa1), // Use of copy cstor
+            new Event(2018, 9, 2),
+            new Festival("science", 23, 8, 2019, 1, 2),
+            new CulturalFiesta(4, "salsa", 20, 10, 2014, 12, 25),
+            new MusicFiesta(23, "Coachella", 600, 1, 2004, 2, 1),
+            new SportCompetition(2, 2018, 8, 4, SportCompetition.Season.SUMMER),
+            new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE)
+        };
+
+        // Print original array
+        for (Event e : list3) {
+            System.out.println(e);
+            System.out.println("----------------------------------------------------------------------");
+        }
+
+        // Create a copy of list3
+        Event[] copiedList3 = copyFestival(list3);
+
+        // Print copied array
+        for (Event e : copiedList3) {
+            System.out.println(e);
+            System.out.println("----------------------------------------------------------------------");
+        }
+        
+        /* As we can see in the output, copiedList3 (coppied array with 12 Event object) is an exact copy of list3 9original array 
+        with 12 Event objects) because by using copy constructors, we were able to create a clone of every single Event object from the original array */
+
+        // Print closing message
+        System.out.println(
+                "\n--------------------------------------------\n"
+                + " Thank you for using our program. Goodbye!\n"
+                + "--------------------------------------------\n");
     }
 
 }
