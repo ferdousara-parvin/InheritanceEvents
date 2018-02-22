3b2c66a3f3a64d9df31d9f33b76f60db1062e256
Viveka Anban <vanban@Vs-MacBook-Pro.local>
2018-02-21 22:19:45 -0500

    Constants done + copyFestival correctted

--- Assignment2/src/package1/Driver.java
+++ Assignment2/src/package1/Driver.java
@@ -4,14 +4,14 @@
 /* Purpose: The purpose of this program is to put into practice some concepts learnt in class such as the concept of inheritance, packages, constructors, 
 access rights, method overriding, etc. For this assignment, we had to develop a program that would help manage the numerous events that are being held or have been held. */
 
-/*
+ /*
 PART 2:
 In Part 1, most of the attributes in parent classes are protected as all the classes in this program (except for the Driver class) are subclasses of Event.
 Therefore, there was no security risks by opening the access rights and, thus, allowing classes from different packages access to Event attributes as all classes are derived from Event class.
 
 However, as there were room for tighter restriction, in Part 2, we changed most of the access rights of all classes to private.
 This meant that whenever we called those attributes by name (super ctor, copy ctor), we had to change the code and use accessors instead.
-*/
+ */
 package package1;
 
 import package2.*;
@@ -26,40 +26,32 @@ public class Driver {
     static final int ZERO = 0;
     static final int ONE = 1;
     static final int TWO = 2;
-    static final int TWO_THOUSAND_AND_EIGHT = 2018;
-    static final int TWO_THOUSAND_AND_NINE = 2019;
+    static final int THREE = 3;
+    static final int FOUR = 4;
+    static final int FIVE = 5;
+    static final int SIX = 6;
+    static final int EIGHT = 8;
+    static final int NINE = 9;
+    static final int TEN = 10;
+    static final int TWELVE = 12;
+    
+    static final int TWENTY = 20;
+    static final int TWENTY_THREE = 23;
+    static final int TWENTY_FIVE = 23;
+    static final int THIRTY = 30;
+    static final int SIX_HUNDRED = 30;
+    
+     
+    static final int TWO_THOUSAND_AND_FIFTEEN = 2015;
+    static final int TWO_THOUSAND_AND_EIGHTEEN = 2018;
+    static final int TWO_THOUSAND_AND_NINETEEN = 2019;
 
-    //Method that returns an array with copies of the events in the original array
+    //Method that is supposed to return an array with copies of the events in the original array
     public static Event[] copyFestival(Event[] eventArray) {
         Event[] copy = new Event[eventArray.length];
-
         for (int i = ZERO; i < copy.length; i++) {
-
-            String className = eventArray[i].getClass().getSimpleName().toLowerCase();
-            
-            //Cast each element in the array to the corresponding derived class of Event
-            switch (className) {
-                case "event":
-                    copy[i] = new Event(eventArray[i]);
-                    break;
-                case "festival":
-                    copy[i] = new Festival((Festival) eventArray[i]);
-                    break;
-                case "culturalfiesta":
-                    copy[i] = new CulturalFiesta((CulturalFiesta) eventArray[i]);
-                    break;
-                case "musicfiesta":
-                    copy[i] = new MusicFiesta((MusicFiesta) eventArray[i]);
-                    break;
-                case "sportcompetition":
-                    copy[i] = new SportCompetition((SportCompetition) eventArray[i]);
-                    break;
-                case "fair":
-                    copy[i] = new Fair((Fair) eventArray[i]);
-                    break;
-            }
+            copy[i] = new Event(eventArray[i]);
         }
-
         return copy;
     }
 
@@ -81,16 +73,16 @@ public class Driver {
 
         //Creation of 6 different events
         Event[] list1 = {
-            new Event(2018, 9, 2),
-            new Festival("arts", 20, 30, 2015, 9, 2),
-            new CulturalFiesta(3, "film", 20, 10, 2008, 12, 25),
-            new MusicFiesta(23, "Justn Bieber", 600, 1, 2018, 2, 28),
-            new SportCompetition(23, 2018, 8, 3, SportCompetition.Season.FALL),
-            new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE)
+            new Event(TWO_THOUSAND_AND_EIGHTEEN, NINE, TWO),
+            new Festival("arts", TWENTY, THIRTY, TWO_THOUSAND_AND_FIFTEEN, NINE, TWO),
+            new CulturalFiesta(THREE, "film", TWENTY, TEN, 2008, TWELVE, TWENTY_FIVE),
+            new MusicFiesta(TWENTY_THREE, "Justn Bieber", SIX_HUNDRED, ONE, TWO_THOUSAND_AND_EIGHTEEN, TWO, 28),
+            new SportCompetition(TWENTY_THREE, TWO_THOUSAND_AND_EIGHTEEN, EIGHT, THREE, SportCompetition.Season.FALL),
+            new Fair(TWENTY_THREE, TWO_THOUSAND_AND_NINETEEN, SIX, TWO, Fair.TypeOfFair.TRADE)
         };
 
         // Print each event in list1
-        for (int i = 0; i < list1.length; i++) {
+        for (int i = ZERO; i < list1.length; i++) {
             System.out.println("#" + i + ". " + list1[i]);
             System.out.println("----------------------------------------------------------------------");
         }
@@ -100,9 +92,9 @@ public class Driver {
         System.out.println("Testing equals method");
         System.out.println("**************************\n");
 
-        Festival fe1 = new Festival("arts", 20, 30, 2015, 9, 2);
-        Fair fa1 = new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE);
-        Fair fa2 = new Fair(23, 2019, 6, 2, Fair.TypeOfFair.SCIENCE);
+        Festival fe1 = new Festival("arts", TWENTY, THIRTY, TWO_THOUSAND_AND_FIFTEEN, NINE, TWO);
+        Fair fa1 = new Fair(TWENTY_THREE, TWO_THOUSAND_AND_NINETEEN, SIX, TWO, Fair.TypeOfFair.TRADE);
+        Fair fa2 = new Fair(TWENTY_THREE, TWO_THOUSAND_AND_NINETEEN, SIX, TWO, Fair.TypeOfFair.SCIENCE);
 
         //Display the compared events
         System.out.println("A. " + fe1);
@@ -111,21 +103,21 @@ public class Driver {
         System.out.println("\nD. " + mf1);
 
         System.out.println("\nAre #0 and A equal? ");
-        System.out.print(list1[0].equals(fe1)); // Two different type of objects
+        System.out.print(list1[ZERO].equals(fe1)); // Two different type of objects
         System.out.println("\n----------------------------------------------------------------------");
 
         System.out.println("Are #3 and D equal? ");
-        System.out.print(list1[3].equals(mf1));// One object is null
+        System.out.print(list1[THREE].equals(mf1));// One object is null
         System.out.println("\n----------------------------------------------------------------------");
 
-        System.out.println("Are #5 and B equal? " + list1[5].equals(fa1)); // Equal objects
+        System.out.println("Are #5 and B equal? " + list1[FIVE].equals(fa1)); // Equal objects
         System.out.println("----------------------------------------------------------------------");
 
-        System.out.println("Are #5 and C equal? " + list1[5].equals(fa2)); // Not equal objects
+        System.out.println("Are #5 and C equal? " + list1[FIVE].equals(fa2)); // Not equal objects
         System.out.println("----------------------------------------------------------------------");
 
         System.out.println("Are #1 and #3 equal? ");
-        System.out.print(list1[1].equals((Festival) list1[3])); // Two different type of objects even when casted
+        System.out.print(list1[ONE].equals((Festival) list1[THREE])); // Two different type of objects even when casted
         System.out.println("\n----------------------------------------------------------------------");
 
         System.out.println();
@@ -133,16 +125,16 @@ public class Driver {
         // Part 1.2
         // Creats events and add them to list2
         Event[] list2 = {
-            new Event(list1[0]),
+            new Event(list1[ZERO]),
             new Festival(fe1), // Use of copy cstor
-            new CulturalFiesta((CulturalFiesta) list1[2]),
-            new MusicFiesta((MusicFiesta) list1[3]),
-            new SportCompetition((SportCompetition) list1[4]),
+            new CulturalFiesta((CulturalFiesta) list1[TWO]),
+            new MusicFiesta((MusicFiesta) list1[THREE]),
+            new SportCompetition((SportCompetition) list1[FOUR]),
             new Fair(fa1), // Use of copy cstor
-            new Event(2018, 9, 2),
-            new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE),
-            new Festival("science", 23, 8, 2019, 1, 2),
-            new SportCompetition(2, 2018, 8, 4, SportCompetition.Season.SUMMER)
+            new Event(TWO_THOUSAND_AND_EIGHTEEN, NINE, TWO),
+            new Fair(TWENTY_THREE, TWO_THOUSAND_AND_NINETEEN, SIX, TWO, Fair.TypeOfFair.TRADE),
+            new Festival("science", TWENTY_THREE, EIGHT, TWO_THOUSAND_AND_NINETEEN, ONE, TWO),
+            new SportCompetition(TWO, TWO_THOUSAND_AND_EIGHTEEN, EIGHT, FOUR, SportCompetition.Season.SUMMER)
         };
 
         System.out.println("//Array of 10 Event objects//");
@@ -165,7 +157,7 @@ public class Driver {
         //PART 1
         /*
         for (int i = ZERO; i < list2.length; i++) {
-            for (int k = 1; k < list2.length; k++) {
+            for (int k = ONE; k < list2.length; k++) {
                 if (list2[i].numberOfCities == list2[leastPopular].numberOfCities || list2[i].numberOfCities == list2[mostPopular].numberOfCities) {
                     break;
                 } else if (list2[i].numberOfCities < list2[k].numberOfCities && list2[i].numberOfCities < list2[leastPopular].numberOfCities) {
@@ -179,7 +171,7 @@ public class Driver {
         //PART 2
         //Accessors and mutators needed for subclasses to access parent classes as all attributes are restricted
         for (int i = ZERO; i < list2.length; i++) {
-            for (int k = 1; k < list2.length; k++) {
+            for (int k = ONE; k < list2.length; k++) {
                 if (list2[i].getNumberOfCities() == list2[leastPopular].getNumberOfCities() || list2[i].getNumberOfCities() == list2[mostPopular].getNumberOfCities()) {
                     break;
                 } else if (list2[i].getNumberOfCities() < list2[k].getNumberOfCities() && list2[i].getNumberOfCities() < list2[leastPopular].getNumberOfCities()) {
@@ -219,7 +211,6 @@ public class Driver {
 
         }
 
-        
         System.out.println(output1 + "\n\n" + output2);
 
         //---------------Events held the same year ---------------------------//
@@ -229,6 +220,8 @@ public class Driver {
 
         int[] yearWithMultipleEventsAndAlreadyPrinted = {ZERO, ZERO, ZERO, ZERO}; // Since in list2, there are four different possibility of years (2008, 2015, 2018, and 2019)
         int index = ZERO;
+
+        int[] indexYears = new int[list2.length];
 
         // Loop through list2
         for (int i = ZERO; i < list2.length; i++) {
@@ -273,23 +266,23 @@ public class Driver {
         System.out.println("//Array of 12 Event objects//");
 
         Event[] list3 = {
-            new Event(list1[0]),
+            new Event(list1[ZERO]),
             new Festival(fe1), // Use of copy cstor
-            new CulturalFiesta((CulturalFiesta) list1[2]),
-            new MusicFiesta((MusicFiesta) list1[3]),
-            new SportCompetition((SportCompetition) list1[4]),
+            new CulturalFiesta((CulturalFiesta) list1[TWO]),
+            new MusicFiesta((MusicFiesta) list1[THREE]),
+            new SportCompetition((SportCompetition) list1[FOUR]),
             new Fair(fa1), // Use of copy cstor
-            new Event(2018, 9, 2),
-            new Festival("science", 23, 8, 2019, 1, 2),
-            new CulturalFiesta(4, "salsa", 20, 10, 2014, 12, 25),
-            new MusicFiesta(23, "Coachella", 600, 1, 2004, 2, 1),
-            new SportCompetition(2, 2018, 8, 4, SportCompetition.Season.SUMMER),
-            new Fair(23, 2019, 6, 2, Fair.TypeOfFair.TRADE)
+            new Event(TWO_THOUSAND_AND_EIGHTEEN, NINE, TWO),
+            new Festival("science", TWENTY_THREE, EIGHT, TWO_THOUSAND_AND_NINETEEN, ONE, TWO),
+            new CulturalFiesta(FOUR, "salsa", TWENTY, TEN, 2014, TWELVE, TWENTY_FIVE),
+            new MusicFiesta(TWENTY_THREE, "Coachella", SIX_HUNDRED, ONE, 2004, TWO, ONE),
+            new SportCompetition(TWO, TWO_THOUSAND_AND_EIGHTEEN, EIGHT, FOUR, SportCompetition.Season.SUMMER),
+            new Fair(TWENTY_THREE, TWO_THOUSAND_AND_NINETEEN, SIX, TWO, Fair.TypeOfFair.TRADE)
         };
 
         // Print original array
         System.out.println("ORIGINAL ARRAY");
-        for (int i = 0; i < list3.length; i++) {
+        for (int i = ZERO; i < list3.length; i++) {
             System.out.println("#" + i + ". " + list3[i]);
             System.out.println("----------------------------------------------------------------------");
         }
@@ -299,13 +292,21 @@ public class Driver {
 
         // Print copied array
         System.out.println("\nCOPY ARRAY");
-        for (int i = 0; i < copiedList3.length; i++) {
+        for (int i = ZERO; i < copiedList3.length; i++) {
             System.out.println("#" + i + ". " + copiedList3[i]);
             System.out.println("----------------------------------------------------------------------");
         }
 
-        /* As we can see in the output, copiedList3 (coppied array with 12 Event object) is an exact copy of list3 9original array 
-         with 12 Event objects) because by using copy constructors, we were able to create a clone of every single Event object from the original array */
+        /*
+        As we can see, this output clearly shows that each one of these events is created as an Event. 
+        By using the Event copy constructor, we create another Event. When using the Event copy ctor, there will be no 
+        polymorphism. Without polymorphism, the compiler will assume that we are indeed only creating copies of 
+        Event classes and will misbehave because we would like copies of the derived classes too. We could try 
+        fixing this problem by trying to find out each element's class and then create and instance of that class 
+        to store in the returned array. However, we would still not be using polymorphism then and such solution 
+        would only work on a small sample (ex. Hard coding is impossible when you have 100 different derived classes).
+        
+         */
         // Print closing message
         System.out.println(
                 "\n--------------------------------------------\n"
